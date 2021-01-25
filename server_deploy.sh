ff592f45e276d5645fcd8126ec325c3b5b2a1af3
Nicolay i <nic.ilinov@gmail.com>
2021-01-25 22:28:30 +0700

    ++

--- server_deploy.sh
+++ server_deploy.sh
@@ -3,27 +3,21 @@ set -e
 
 echo "Deploying application ..."
 
-# Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
-    # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+(
+    git fetch origin deploy &&
+        git reset --hard origin/deploy &&
+        composer install --no-interaction --prefer-dist --optimize-autoloader &&
+        php artisan migrate --force &&
+        php artisan optimize &&
+        php artisan up &&
+        sudo service apache2 restart &&
+        echo "Application deployed!" &&
+        ../done.sh 2
+)
 
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+    echo "We have an error"
 
-    # Migrate database
-    php artisan migrate --force
-
-    # Note: If you're using queue workers, this is the place to restart them.
-    # ...
-
-    # Clear cache
-    php artisan optimize
-
-    # Reload PHP to update opcache
-    echo "" | sudo service apache2 restart
-# Exit maintenance mode
-php artisan up
-
-echo "Application deployed!"
+    exit $errorCode
+fi
